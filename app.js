4e8cc4746f71e7a2959111927e1cd98ff9887e32
EmirVelazquez <emirleovelazquez@gmail.com>
2020-04-24 22:01:04 -0500

    modified app.js file. added all sections necessary for fcc testing to pass all tests.

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("app.js working")
-})
+    // ==================================================
+    // References to elements or Global variables
+    // ==================================================
+    const navBar = document.getElementById("nav-bar");
+
+    // ==================================================
+    // Main Executions
+    // ==================================================
+    window.onscroll = function () {
+        console.log(navBar);
+    }
+
+    // ==================================================
+    // Helper Functions
+    // ==================================================
+});
