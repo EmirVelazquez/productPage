574f33a20e717d9f178b2a9258c9862aca646c88
EmirVelazquez <emirleovelazquez@gmail.com>
2020-04-27 22:56:50 -0500

    added logic to handle the display of freecodecamps test script button. added new company logo img asset. css improvements and changes.

--- app.js
+++ app.js
@@ -2,16 +2,29 @@ document.addEventListener("DOMContentLoaded", function () {
     // ==================================================
     // References to elements or Global variables
     // ==================================================
-    const navBar = document.getElementById("nav-bar");
+    const fccButton = document.getElementById('fcc_test_suite_wrapper');
+    const fccButtonToggle = document.getElementById("scriptTest");
+    // Flag for toggle
+    var showing = false;
 
     // ==================================================
     // Main Executions
     // ==================================================
-    window.onscroll = function () {
-        console.log(navBar);
-    }
+    toggleTest();
 
     // ==================================================
     // Helper Functions
     // ==================================================
+    // This function handles the toggle for the test script from freeCodeCamp
+    function toggleTest() {
+        fccButtonToggle.addEventListener("click", function () {
+            if (showing) {
+                fccButton.style.display = "none";
+                showing = false;
+            } else {
+                fccButton.style.display = "block";
+                showing = true;
+            }
+        });
+    }
 });
